a27c6b6e9a5087a44f6aef5478d5a464a13eeea0
Tsjerk A. Wassenaar <tsjerkw@gmail.com>
2016-05-17 21:46:10 +0200

    Added index function to martinate - modified from gromit for CG (fixes issue #7)

--- martinate.sh
+++ martinate.sh
@@ -416,8 +416,7 @@ DSSP=$(find_program_fun dssp)
 if [[ $? == 1 ]]
 then
     warn="DSSP binary not found - Will martinize without secondary structure :S"
-    store_warn_fun "$warn"
-    WARN "$warn"
+    store_warning_fun "$warn"
 else
     echo "$DSSP"
     MARTINIZE+=(-dssp=$DSSP)
@@ -1046,6 +1045,97 @@ program_options()
         fi
     done
     echo $OPTS
+}
+
+# Amino acids
+amino_acids=(ALA CYS ASP GLU PHE GLY HIS HIH ILE LYS LEU MET ASN PRO GLN ARG SER THR VAL TRP TYR)
+function sed_protein()
+{
+    for a in ${amino_acids[@]}
+    do 
+	echo '-e /'$a/$1
+    done
+}
+
+# Nucleic acids
+nucleic_acids=(DG DA DC DT)
+function sed_nucleic()
+{
+    for a in ${nucleic_acids[@]}
+    do 
+	echo '-e /'$a/$1
+    done
+}
+
+# Solvent
+solvent_names=(W\ \+W PW BMW SOL)
+function sed_solvent()
+{
+    for a in ${solvent_names[@]}
+    do 
+	echo '-e /'$a/$1
+    done
+}
+
+function INDEX()
+{
+    [[ -n $2 ]] && fn=$2 || fn=basic.ndx
+ 
+    exec 6>&1 && exec >$fn
+
+    fmt="%5d %5d %5d %5d %5d %5d %5d %5d %5d %5d"
+
+    # Whatever we do not have explicitly must be membrane
+
+    # Total number of atoms
+    N=$(awk '{getline; print; exit}' $1)
+    echo "[ System ]"
+    printf "$fmt\n" `SEQ 1 $N` | $SED 's/ 0//g'
+    
+    # Solvent atoms (including ions, etc, listed after 'SOL/W/...')
+    SOL=$(( $($SED -n $(sed_solvent '{=;q;}') $1) - 2 ))
+    echo "[ Solvent ]"
+    printf "$fmt\n" `SEQ $SOL $N` | $SED 's/ 0//g'
+    
+    # Base system: solute and membrane, if present
+    echo "[ Base ]"
+    printf "$fmt\n" `SEQ 1 $((SOL - 1))` | $SED 's/ 0//g'
+    
+    # Protein - Get the first and last atom of protein stuff
+    PROTSTART=$(( $($SED -n $(sed_protein '{=;q;}') $1) - 2 ))
+    PROTEND=$(( $($SED -n -e 1,2d $(sed_protein d) -e '{=;q;}' $1) - 3 )) 
+    echo "[ Protein ]"
+    [[ $PROTSTART -gt 0 ]] && printf "$fmt\n" `SEQ $PROTSTART $PROTEND` | $SED 's/ 0//g'
+
+    # Protein - Get the first and last atom of protein stuff
+    NUCSTART=$(( $($SED -n $(sed_nucleic '{=;q;}') $1) - 2 ))
+    NUCEND=$(( $($SED -n -e 1,2d $(sed_nucleic d) -e '{=;q;}' $1) - 3 )) 
+    echo "[ Nucleic ]"
+    [[ $NUCSTART -gt 0 ]] && printf "$fmt\n" `SEQ $NUCSTART $NUCEND` | $SED 's/ 0//g'
+
+    # Membrane, if any
+    MEMBRANE=$(( $($SED -n -e '1,2d' $(sed_protein d) $(sed_nucleic d) -e '{=;q;}' $1) - 2 ))
+    echo '[ Membrane ]'
+    if [[ -n $MEMBRANE && $MEMBRANE -gt 0 && $MEMBRANE != $SOL ]]
+    then
+        printf "$fmt\n" `SEQ $MEMBRANE $((SOL - 1))` | $SED 's/ 0//g'
+    fi
+
+    echo '[ Solute ]'
+    if [[ $PROTSTART -gt 0 && $NUCSTART -gt 0 ]]
+    then
+	printf "$fmt\n" `SEQ $PROTSTART $NUCEND` | $SED 's/ 0//g'
+    elif [[ $PROTSTART -gt 0 ]]
+    then
+	printf "$fmt\n" `SEQ $PROTSTART $PROTEND` | $SED 's/ 0//g'
+    elif [[ $NUCSTART -gt 0 ]]
+    then
+	printf "$fmt\n" `SEQ $NUCSTART $NUCEND` | $SED 's/ 0//g'
+    fi
+	
+    exec 1>&6 6>&-
+
+    return 0
 }
 
 
@@ -1845,8 +1935,7 @@ then
     if [[ -n $SOL && ! "$INSANE" =~ " -sol " ]]
     then
 	note_msg="No solvent included in insane statement... Adding \"-sol $SOL\""
-	store_note_fun "$note"
-	NOTE "$note"
+	store_note_fun "$note_msg"
 	INSANE="$INSANE -sol $SOL"
     fi
 
