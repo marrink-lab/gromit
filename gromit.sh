4f7200bf78fa20f94f791eb59fba47f99c812670
Tsjerk A. Wassenaar <tsjerkw@gmail.com>
2018-07-16 11:18:46 +0200

    Equalized handling user specified MDP options and removed tabs

--- gromit.sh
+++ gromit.sh
@@ -1014,25 +1014,26 @@ fi
 # This function lists the mdp options requested based on a preceding tag
 function mdp_options ()
 {    
-    for tag in $@
+  echo MDP tags: $@ 1>&2
+  for tag in $@
+  do
+  # Find variables declared with specified tag
+    for opt in `set | grep ^__mdp_${tag}__`
     do
-	# Find variables declared with specified tag
-        for opt in `set | grep ^__mdp_${tag}__`
-        do
-	    # Strip everything from the first = to the end 
-	    # to get the variable name
-            var=${opt%%=*}
-	    # Strip everything up to the first = to get
-	    # the value
-            val=${opt#*=}
-	    # Replace the tag and redeclare in local space
-	    # If the variable was already declared it will
-	    # be overridden.
-            local ${var/mdp_$tag/mdp}=$val
-        done
+      # Strip everything from the first = to the end 
+      # to get the variable name
+      var=${opt%%=*}
+      # Strip everything up to the first = to get
+      # the value
+      val=${opt#*=}
+      # Replace the tag and redeclare in local space
+      # If the variable was already declared it will
+      # be overridden.
+      local ${var/mdp_$tag/mdp}=$val
     done
-    # Find all variables starting with __mdp__ and echo them
-    set | $SED -n '/^__mdp__/{s/__mdp__//;s/,/ /g;p;}'
+  done
+  # Find all variables starting with __mdp__ and echo them
+  set | $SED -n '/^__mdp__/{s/__mdp__//;s/,/ /g;p;}'
 }
 
 
@@ -1160,41 +1161,39 @@ __mdp_equil__gen_temp=${Temperature[0]}
 
 #--------------------------------------------------------------------
 # User specified parameters
-# These will be used in PR-NVT, PR-NPT, MD-INIT, MD-PRE and MD
-# To specify parameters for a single step, make cunning use of
-# -step and -stop.
+
 
 if [[ -n $MDP ]]
 then
-    # Check if the MDP file specified exists and exit if not
-    [[ ! -f $MDP ]] && echo "MDP file $MDP specified, but not found" && exit
+  # Check if the MDP file specified exists and exit if not
+  [[ ! -f $MDP ]] && echo "MDP file $MDP specified, but not found" && exit
+
+  # Gather options
+  # 1. Delete empty lines
+  # 2. Delete comment lines
+  # 3. Remove whitespace on either side of the equality sign
+  # 4. Remove trailing whitespace
+  # 5. Replace spaces with commas
+  USER_MDP=( $( $SED '/^ *$/d;/^ *;/d;s/\s*=\s*/=/;s/\s*$//;s/ /,/g;' $MDP ) )
     
-    # Gather options
-    # 1. Delete empty lines
-    # 2. Delete comment lines
-    # 3. Remove whitespace on either side of the equality sign
-    # 4. Remove trailing whitespace
-    # 5. Replace spaces with commas
-    USER_MDP=( $( $SED '/^ *$/d;/^ *;/d;s/\s*=\s*/=/;s/\s*$//;s/ /,/g;' $MDP ) )
-    
-    for i in ${USER_MDP[@]}
-    do 
-	opt=${i%%=*}
-	val=${i#$opt}
-	eval "__mdp_usr__${opt//-/_}$val"
-    done    
+  for i in ${USER_MDP[@]}
+  do 
+    opt=${i%%=*}
+    val=${i#$opt}
+    eval "__mdp_usr__${opt//-/_}$val"
+  done    
 fi
 
 
 if [[ -n $MDPOPTS ]]
 then
-    for i in ${MDPOPTS[@]}
-    do
-        # --mdp-energygrps=bla,bla,bla
-        opt=${i%%=*}
-        val=${i#$opt}
-        eval "__mdp_usr__${opt//-/_}$val"
-    done
+  for i in ${MDPOPTS[@]}
+  do
+    # --mdp-energygrps=bla,bla,bla
+    opt=${i%%=*}
+    val=${i#$opt}
+    eval "__mdp_usr__${opt//-/_}$val"
+  done
 fi
 
 
--- martinate.sh
+++ martinate.sh
@@ -973,29 +973,30 @@ echo Done gymnastics
 ## E OT ## The list as is was set up for gromacs 4.5 and 5.1
 
 # This function lists the mdp options requested based on a preceding tag
-mdp_options ()
-{
-    echo MDP tags: $@ 1>&2
-    for tag in $@
+function mdp_options ()
+{    
+  echo MDP tags: $@ 1>&2
+  for tag in $@
+  do
+  # Find variables declared with specified tag
+    for opt in `set | grep ^__mdp_${tag}__`
     do
-        # Find variables declared with specified tag
-        for opt in `set | grep ^__mdp_${tag}__`
-        do
-	    # Strip everything from the first = to the end 
-	    # to get the variable name
-            var=${opt%%=*}
-	    # Strip everything up to the first = to get
-	    # the value
-            val=${opt#*=}
-            # Replace the tag and redeclare in local space
-            # If the variable was already declared it will
-            # be overridden.
-            local ${var/mdp_$tag/mdp}=$val
-        done
+      # Strip everything from the first = to the end 
+      # to get the variable name
+      var=${opt%%=*}
+      # Strip everything up to the first = to get
+      # the value
+      val=${opt#*=}
+      # Replace the tag and redeclare in local space
+      # If the variable was already declared it will
+      # be overridden.
+      local ${var/mdp_$tag/mdp}=$val
     done
-    # Find all variables starting with __mdp__ and echo them
-    set | sed -n '/^__mdp__/{s/__mdp__//;s/,/ /g;p;}'
+  done
+  # Find all variables starting with __mdp__ and echo them
+  set | $SED -n '/^__mdp__/{s/__mdp__//;s/,/ /g;p;}'
 }
+
 
 #--------------------------------------------------------------------
 # Global parameters
@@ -1143,35 +1144,35 @@ __mdp_equil__tau_p=10
 
 if [[ -n $MDP ]]
 then
-    # Check if the MDP file specified exists and exit if not
-    [[ ! -f $MDP ]] && echo "MDP file $MDP specified, but not found" && exit
+  # Check if the MDP file specified exists and exit if not
+  [[ ! -f $MDP ]] && echo "MDP file $MDP specified, but not found" && exit
+
+  # Gather options
+  # 1. Delete empty lines
+  # 2. Delete comment lines
+  # 3. Remove whitespace on either side of the equality sign
+  # 4. Remove trailing whitespace
+  # 5. Replace spaces with commas
+  USER_MDP=( $( $SED '/^ *$/d;/^ *;/d;s/\s*=\s*/=/;s/\s*$//;s/ /,/g;' $MDP ) )
     
-    # Gather options
-    # 1. Delete empty lines
-    # 2. Delete comment lines
-    # 3. Remove whitespace on either side of the equality sign
-    # 4. Remove trailing whitespace
-    # 5. Replace spaces with commas
-    USER_MDP=( $( sed '/^ *$/d;/^ *;/d;s/\s*=\s*/=/;s/\s*$//;s/ /,/g;' $MDP ) )
-    
-    for i in ${USER_MDP[@]}
-    do 
-        opt=${i%%=*}
-        val=${i#$opt}
-        eval "__mdp_usr__${opt//-/_}$val"
-    done    
+  for i in ${USER_MDP[@]}
+  do 
+    opt=${i%%=*}
+    val=${i#$opt}
+    eval "__mdp_usr__${opt//-/_}$val"
+  done    
 fi
 
 
 if [[ -n $MDPOPTS ]]
 then
-    for i in ${MDPOPTS[@]}
-    do
-        # --mdp-energygrps=bla,bla,bla
-        opt=${i%%=*}
-        val=${i#$opt}
-        eval "__mdp_usr__${opt//-/_}$val"
-    done
+  for i in ${MDPOPTS[@]}
+  do
+    # --mdp-energygrps=bla,bla,bla
+    opt=${i%%=*}
+    val=${i#$opt}
+    eval "__mdp_usr__${opt//-/_}$val"
+  done
 fi
 
 
