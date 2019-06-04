18cf5889e8471f6637e43f23e1bce4d8452a0c07
Tsjerk <tsjerkw@gmail.com>
2019-06-04 11:07:01 +0200

    Moved logging functions from gromit to _logging.sh

--- gromit.sh
+++ gromit.sh
@@ -171,20 +171,8 @@ get_step_fun() { for ((i=0; i<${#STEPS[@]}; i++)) do [[ ${STEPS[$i]} =~ ^$1 ]] &
 STEP=
 STOP=PRODUCTION
 
-# Macros to echo stuff only if the step is to be executed -- more about steps further down
-RED='\x1b[1;31m'
-YEL='\x1b[1;33m'
-GRN='\x1b[1;32m'
-CYA='\x1b[1;36m'
-OFF='\x1b[0m'
-LINES() { sed -e 's/^/ /;s/$/ /;h;s/./-/g;p;x;p;x;' <<< "$@"; }
-SHOUT() { [[ $STEP == $NOW ]] && echo && LINES "$@" | sed 's/^/#/'; }
-WARN()  { [[ $STEP == $NOW ]] && echo -e "$RED" && LINES "$@" | sed 's/^/# WARNING: /' && echo -e "$OFF"; }
-FATAL() { [[ $STEP == $NOW ]] && echo -e "$RED" && LINES "$@" | sed 's/^/# FATAL: /' && echo -e "$OFF"; exit 1; }
-NOTE()  { [[ $STEP == $NOW ]] && echo -e "$YEL" && LINES "$@" | sed 's/^/# NOTE: /' && echo -e "$OFF"; }
-LINE()  { [[ $STEP == $NOW ]] && echo && sed -e 's/^/ /;s/$/ /p;s/./-/g;' <<< "$@" | sed 's/^/#/'; }
-ECHO()  { [[ $STEP == $NOW ]] && echo "$@"; }
-
+# Sourcing stuff
+source "$SDIR"/_logging.sh
 
 # Force field
 ForceFieldFamilies=(gromos  charmm  amber   opls )
@@ -1875,6 +1863,14 @@ fi
 
 # a. Ions
 # b. Ligands/Factors
+hetstuff=($(awk '/^HETNAM/{print $2}' $dirn/$pdb))
+hetsed=$(echo ${hetstuff[@]} | sed 's/ \+/\\|/g')
+sed '/^HETATM.\{11,12\}\('$hetsed'\)/d' $pdb | sed /DUM/d > nohet.pdb
+for het in ${hetstuff[@]}
+do
+    grep $het $pdb > $het.pdb
+done
+#exit
 
 ## III. Nucleic acids
 
