bdd96472e4767ce9ad7d2af2bca64bd4815fadb2
Tsjerk <tsjerkw@gmail.com>
2019-06-05 14:14:10 +0200

    Moved other MDP stuff (function/user) from martinate and gromit to _mdp.sh

--- martinate.sh
+++ martinate.sh
@@ -841,74 +841,8 @@ $M && TABLES=-tables || TABLES=
 ## NOTE ## This is probably fine for equilibration, but check the defaults to be sure
 ## E OT ## The list as is was set up for gromacs 4.5 and 5.1
 
-# This function lists the mdp options requested based on a preceding tag
-function mdp_options ()
-{    
-  echo MDP tags: $@ 1>&2
-  for tag in $@
-  do
-  # Find variables declared with specified tag
-    for opt in `set | grep ^__mdp_${tag}__`
-    do
-      # Strip everything from the first = to the end 
-      # to get the variable name
-      var=${opt%%=*}
-      # Strip everything up to the first = to get
-      # the value
-      val=${opt#*=}
-      # Replace the tag and redeclare in local space
-      # If the variable was already declared it will
-      # be overridden.
-      local ${var/mdp_$tag/mdp}=$val
-    done
-  done
-  # Find all variables starting with __mdp__ and echo them
-  set | $SED -n '/^__mdp__/{s/__mdp__//;s/,/ /g;p;}'
-}
-
-
-#--------------------------------------------------------------------
-# Global parameters
-
 source "${SRCDIR}"/_mdp_martinate.sh
-
-#--------------------------------------------------------------------
-# User specified parameters
-
-
-if [[ -n $MDP ]]
-then
-  # Check if the MDP file specified exists and exit if not
-  [[ ! -f $MDP ]] && echo "MDP file $MDP specified, but not found" && exit
-
-  # Gather options
-  # 1. Delete empty lines
-  # 2. Delete comment lines
-  # 3. Remove whitespace on either side of the equality sign
-  # 4. Remove trailing whitespace
-  # 5. Replace spaces with commas
-  USER_MDP=( $( $SED '/^ *$/d;/^ *;/d;s/\s*=\s*/=/;s/\s*$//;s/ /,/g;' $MDP ) )
-    
-  for i in ${USER_MDP[@]}
-  do 
-    opt=${i%%=*}
-    val=${i#$opt}
-    eval "__mdp_usr__${opt//-/_}$val"
-  done    
-fi
-
-
-if [[ -n $MDPOPTS ]]
-then
-  for i in ${MDPOPTS[@]}
-  do
-    # --mdp-energygrps=bla,bla,bla
-    opt=${i%%=*}
-    val=${i#$opt}
-    eval "__mdp_usr__${opt//-/_}$val"
-  done
-fi
-
+source "${SRCDIR}"/_mdp.sh
 
 #--------------------------------------------------------------------
 
